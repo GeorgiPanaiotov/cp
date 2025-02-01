8bb90bfed13583c43ecbd86bbe2333034cf28e09
Georgi Panayotov <rippness@gmail.com>
2025-02-01 11:15:07 +0000

    Implement error handling for reading and writing fails and exit with success instead of 0 for consistency

--- copy.c
+++ copy.c
@@ -14,27 +14,30 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	int wrfd = open(argv[2], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+	int wrfd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
 	if (wrfd == -1) {
 		perror("Unable to create file");
+		close(rdfd);
 		exit(EXIT_FAILURE);
 	}
 
 	ssize_t br;
 
 	while((br = read(rdfd, buffer, MAX_COUNT)) > 0) {
-		buffer[br] = '\0';
-		ssize_t bw = write(wrfd, buffer, MAX_COUNT);
-
-		if (bw == -1) {
-			perror("Cannot write to the file!");
+		ssize_t bw = write(wrfd, buffer, br);
+		if (bw != br) {	
+			perror("Unable to write to file");
+			close(rdfd);
+			close(wrfd);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	if (br == -1) {
-		perror("Cannot read file");
+	if (br == -1) {	
+		perror("Unable to read file");
+		close(rdfd);
+		close(wrfd);
 		exit(EXIT_FAILURE);
 	}
 
